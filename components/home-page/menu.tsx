30febc7e48867ba424a638c2a6c500bd24b85627
Hamed Tahmasebi <87143540+hamedtahmasebi@users.noreply.github.com>
2022-06-18 00:47:14 +0430

    Home / Menu / add active links styles + dynamize routes

---
+++ components/home-page/menu.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import React from "react";
+import { IRoute } from "./side-bar";
+import { useRouter } from "next/router";
+interface Props {
+    routes: IRoute[];
+    close: () => void;
+}
+
+export const Menu: React.FC<Props> = ({ routes, close }) => {
+    const url = useRouter();
+    return (
+        <div className="flex">
+            <div
+                className={`bg-white shadow-lg rounded-r-2xl md:rounded-none dark:bg-slate-800 h-screen flex flex-col
+                 md:flex-row px-5 py-6 md:px-24 md:h-auto absolute md:relative w-3/4 md:w-full
+                 text-primary-400 font-bold text-primary-main  dark:text-primaryDark-main`}
+            >
+                <div className="text-3xl md:text-5xl ">Logo</div>
+                <ul className="flex flex-col md:flex-row justify-center md:items-end mt-8 md:mt-0 md:ml-8">
+                    {routes.map((route, index) => (
+                        <li
+                            key={`route-${index}`}
+                            className={`
+                            transition-all duration-150 rounded-2xl  my-2 md:mx-6
+                            hover:bg-primary-300 hover:text-white
+                          ${
+                              url.asPath === route.link &&
+                              "bg-primary-300 dark:bg-primaryDark-300 text-white"
+                          }
+                          hover:dark:bg-primaryDark-main hover:dark:text-black
+                          `}
+                        >
+                            <Link href={route.link} key={`route-${index}`}>
+                                <a
+                                    className="block p-2 px-4  w-full text-base md:text-xl "
+                                    onClick={(e) => close()}
+                                >
+                                    {route.title}
+                                </a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div
+                className={`md:hidden absolute right-0 w-1/4 h-screen 
+                bg-primary-200 dark:bg-black bg-opacity-5 dark:bg-opacity-30 blur-sm`}
+                onClick={(e) => close()}
+            ></div>
+        </div>
+    );
+};
+
+export default Menu;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,11 @@
 import type { NextPage } from "next";
-import Menu from "../components/menu";
-import { useRef, useState } from "react";
-import { BsX } from "react-icons/bs";
-import { RiMenuFill, RiSearch2Line } from "react-icons/ri";
 import { Intro } from "../components/home-page/intro";
-const routes = [
-    {
-        title: "Home",
-        link: "#home",
-    },
-    {
-        title: "Shop",
-        link: "#shop",
-    },
-    {
-        title: "Sign up",
-        link: "#signup",
-    },
-];
+import { useRef, useState } from "react";
 
 const Home: NextPage = () => {
-    const [openMenu, setOpenMenu] = useState<Boolean>(false);
-    const menuContainer = useRef(null);
-
-    const toggleMenu = (menuElement: HTMLDivElement) => {
-        menuElement.classList.toggle("w-3/4");
-    };
-
     return (
         <div className="w-full h-full relative">
-            <div
-                className={`md:hidden w-full shadow-lg -top-1 bg-white dark:bg-slate-900
-                a flex items-center justify-between py-4 px-6 text-primary-main dark:text-primaryDark-main`}
-            >
-                <label className="">
-                    <input
-                        type="checkbox"
-                        name="menu-toggle"
-                        id=""
-                        hidden
-                        onChange={(e) => setOpenMenu(!openMenu)}
-                    />
-                    <span className="text-[2rem]">
-                        <RiMenuFill />
-                    </span>
-                </label>
-                <div className="text-4xl">Logo</div>
-                <div className="text-[2rem]">
-                    <RiSearch2Line />
-                </div>
-            </div>
-
-            <div
-                ref={menuContainer}
-                className={`md:hidden fixed w-full top-0 ease-in-out duration-300 ${
-                    openMenu ? "translate-x-0" : "-translate-x-full"
-                }`}
-                onClick={(e) => toggleMenu(e.currentTarget)}
-            >
-                <Menu routes={routes} close={() => setOpenMenu(false)} />
-            </div>
-            <div className="hidden md:block">
-                <Menu routes={routes} close={() => setOpenMenu(false)} />
-            </div>
-            <div className="flex justify-center">
+            <div className="flex justify-center mt-3">
                 <Intro />
             </div>
         </div>
