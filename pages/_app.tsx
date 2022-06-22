8f18e94e7bf9595a8d8bd609fadb6a316ebdb600
Hamed Tahmasebi <87143540+hamedtahmasebi@users.noreply.github.com>
2022-06-22 17:19:48 +0430

    FIX / move dark mode initializer to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { useEffect } from "react";
+import { switchDarkMode } from "../utilities/actions";
 function MyApp({ Component, pageProps }: AppProps) {
+    function darkModeInitializer() {
+        let darkModeStatus = localStorage.getItem("darkMode");
+        console.log(darkModeStatus);
+        if (!darkModeStatus || darkModeStatus === "false") return switchDarkMode("off");
+        if (darkModeStatus && darkModeStatus === "true") return switchDarkMode("on");
+    }
+    useEffect(() => {
+        darkModeInitializer();
+    });
     return (
         <Layout>
             <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,6 @@ import { switchDarkMode } from "../utilities/actions";
 import { useEffect } from "react";
 
 const Home: NextPage = () => {
-    function darkModeInitializer() {
-        let darkModeStatus = localStorage.getItem("darkMode");
-        console.log(darkModeStatus);
-        if (!darkModeStatus || darkModeStatus === "false") return switchDarkMode("off");
-        if (darkModeStatus && darkModeStatus === "true") return switchDarkMode("on");
-    }
-    useEffect(() => {
-        darkModeInitializer();
-    });
-
     return (
         <div className="w-full h-full relative">
             <div className="flex justify-center mt-3">
