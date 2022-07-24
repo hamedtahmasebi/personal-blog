c17037ee60567b33efc18c8123fa7b54852a4057
Hamed Tahmasebi <87143540+hamedtahmasebi@users.noreply.github.com>
2022-07-24 19:24:50 +0430

    Layout / UI / refactor navbar positioning css

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,11 +3,13 @@ import NavigationBar from "./navigation-bar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            <div className="flex xl:mx-28 2xl:mx-44 w-auto h-full relative">
-                <div className="fixed -bottom-1 w-full md:relative md:w-auto md:px-6 z-50">
-                    <NavigationBar />
+            <div className="flex xl:mx-28 2xl:mx-44 w-auto h-full relative pb-36 md:pb-0">
+                <div className="relative w-auto md:px-6 z-50">
+                    <div className="fixed md:sticky w-full bottom-0  ">
+                        <NavigationBar />
+                    </div>
                 </div>
-                <div className="w-full border-l border-r px-8 md:px-12 lg:px-16 xl:px-24">
+                <div className="w-full border-l border-r px-4 md:px-12 lg:px-16 xl:px-24">
                     {children}
                 </div>
                 <div className="hidden lg:flex lg:w-4/12 xl:w-3/12">Other things</div>
